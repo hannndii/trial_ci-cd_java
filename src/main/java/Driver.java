2699f1a91c0e544c080de52fa0dee6bb6063f3d3
hannndii <shiirogami95@gmail.com>
2026-06-12 12:32:29 +0700

    first commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Currentt count:" + counter.getCount());
 		
 		counter.increment();
 		
